b203b82a608233bdc1ff0b51c17287f37d86ff53
Prakhar Gupta <gprakhar141@gmail.com>
2024-03-31 00:33:41 +0530

    Implement all routes for REST API with authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,19 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs"
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
+const getUserProfile = async(req,res) => {
+    const {username} = req.params;
+    try {
+        const user = await User.findOne({username}).select("-password").select("-updatedAt")
+        if(!user) return res.status(400).json({message: "User not found"})
+        
+        else res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        console.log("Error in getUserProfile: ",error.message);
+    }
+}
+
 const signupUser = async(req,res) => {
     try {
         const {name,email,username,password} = req.body;
@@ -137,4 +150,4 @@ const updateUser = async(req,res) => {
     }
 }
 
-export {signupUser,loginUser,logoutUser,followUnFollowUser,updateUser};
+export {signupUser,loginUser,logoutUser,followUnFollowUser,updateUser,getUserProfile};
